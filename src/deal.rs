be3047193c8699f69adc552684fc1dfcee765831
Tumas Bajoras <tumas.bajoras@gmail.com>
2021-12-30 14:58:16 +0200

    avoid duplicate entries, also other minor fixes

--- src/deal.rs
+++ src/deal.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use log::info;
 use regex::Regex;
 use reqwest::Client;
@@ -17,45 +19,6 @@ pub struct Banner {
 pub struct HomeResponse {
     #[serde(default, rename(deserialize = "homeGridBanners"))]
     banners: Vec<Banner>,
-}
-
-async fn fetch_home_reponse(client: &Client) -> Result<HomeResponse, reqwest::Error> {
-    let resp = client.get(BASE_URL).send().await?;
-    resp.json::<HomeResponse>().await
-}
-
-async fn ping(client: &reqwest::Client, url: &str) -> Result<bool, reqwest::Error> {
-    let response = client.head(url).send().await?;
-    let status = response.status().is_success();
-
-    if status {
-        green_ln!("URL: {}, success: {}", url, status)
-    } else {
-        red_ln!("URL: {}, success: {}", url, status);
-    }
-
-    Ok(status)
-}
-
-fn candidates(base_url: &str) -> Vec<String> {
-    let mut results = vec![];
-    let add_dd = |url: &str| url.replace("-de-", "-de-dd-");
-
-    // no change, de -> de-dd
-    results.push(add_dd(base_url));
-
-    // without numbers, de -> de-dd
-    let mut parts: Vec<&str> = base_url.split("/").collect();
-
-    if let Some(title) = parts.pop() {
-        let re = Regex::new(r"[0-9]").expect("Number regexp must be valid");
-        let title = re.replace_all(title, "").to_string();
-        let result = add_dd(&format!("{}/{}", parts.join("/"), title));
-
-        results.push(result);
-    }
-
-    results
 }
 
 pub async fn find(stop_when_found: bool) -> Result<(), reqwest::Error> {
@@ -87,7 +50,7 @@ pub async fn find(stop_when_found: bool) -> Result<(), reqwest::Error> {
     Ok(())
 }
 
-pub async fn find_by_banners(
+async fn find_by_banners(
     client: &Client,
     response: &HomeResponse,
     stop_when_found: bool,
@@ -108,7 +71,74 @@ pub async fn find_by_banners(
     Ok(false)
 }
 
-pub async fn check_url(
+async fn find_by_iteration(
+    client: &Client,
+    template: &str,
+    stop_when_found: bool,
+) -> Result<bool, reqwest::Error> {
+    let whiskeys = offers::whiskeys().await?;
+    let brands = whiskeys
+        .iter()
+        .filter_map(|whiskey| whiskey.brand())
+        .map(|brand| brand.to_lowercase().replace(" ", "-"))
+        .collect::<HashSet<String>>();
+
+    let mut brand_list = brands.into_iter().collect::<Vec<String>>();
+
+    brand_list.sort();
+
+    for brand in brand_list {
+        let url = template.replace("{}", &brand);
+        let success = check_url(client, &url, stop_when_found).await?;
+
+        if stop_when_found && success {
+            return Ok(success);
+        }
+    }
+
+    Ok(false)
+}
+
+async fn fetch_home_reponse(client: &Client) -> Result<HomeResponse, reqwest::Error> {
+    let resp = client.get(BASE_URL).send().await?;
+    resp.json().await
+}
+
+async fn ping(client: &reqwest::Client, url: &str) -> Result<bool, reqwest::Error> {
+    let response = client.head(url).send().await?;
+    let status = response.status().is_success();
+
+    if status {
+        green_ln!("URL: {}, success: {}", url, status)
+    } else {
+        red_ln!("URL: {}, success: {}", url, status);
+    }
+
+    Ok(status)
+}
+
+fn candidates(base_url: &str) -> HashSet<String> {
+    let mut results = HashSet::new();
+    let add_dd = |url: &str| url.replace("-de-", "-de-dd-");
+
+    // no change, de -> de-dd
+    results.insert(add_dd(base_url));
+
+    // without numbers after de -> de-dd
+    let mut parts: Vec<&str> = base_url.split("/").collect();
+
+    if let Some(title) = parts.pop() {
+        let re = Regex::new(r"(?P<prefix>-de-[^0-9,-]*)-*[0-9]*-*").expect("Regexp must be valid");
+        let title = re.replace(title, "${prefix}").to_string();
+        let result = add_dd(&format!("{}/{}", parts.join("/"), title));
+
+        results.insert(result);
+    }
+
+    results
+}
+
+async fn check_url(
     client: &Client,
     url: &str,
     stop_when_found: bool,
@@ -128,42 +158,84 @@ pub async fn check_url(
     Ok(false)
 }
 
-pub async fn find_by_iteration(
-    client: &Client,
-    template: &str,
-    stop_when_found: bool,
-) -> Result<bool, reqwest::Error> {
-    let whiskeys = offers::whiskeys().await?;
-    let brands = whiskeys
-        .iter()
-        .filter_map(|whiskey| whiskey.brand())
-        .map(|brand| brand.to_lowercase().replace(" ", "-"))
-        .collect::<Vec<String>>();
-
-    for brand in brands {
-        let url = template.replace("{}", &brand);
-        let success = check_url(client, &url, stop_when_found).await?;
-
-        if stop_when_found && success {
-            return Ok(success);
-        }
-    }
-
-    Ok(false)
-}
-
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
+
     use super::candidates;
 
     #[test]
-    fn tests_candidates() {
+    fn tests_candidates_without_number() {
+        let mut expected = HashSet::new();
+        expected
+            .insert("homepage/drankdozijn/drankdozijn-enjoyislay-de-dd-lagavulin.jpg".to_string());
+
         assert_eq!(
-            candidates("homepage/drankdozijn/drankdozijn-enjoyislay20-de-lagavulin.jpg"),
-            vec![
-                "homepage/drankdozijn/drankdozijn-enjoyislay20-de-dd-lagavulin.jpg",
-                "homepage/drankdozijn/drankdozijn-enjoyislay-de-dd-lagavulin.jpg"
-            ]
-        )
+            candidates("homepage/drankdozijn/drankdozijn-enjoyislay-de-lagavulin.jpg"),
+            expected
+        );
+    }
+
+    #[test]
+    fn tests_candidates_with_single_digit_number() {
+        let mut expected = HashSet::new();
+
+        expected
+            .insert("homepage/drankdozijn/drankdozijn-enjoyislay-de-dd-lagavulin8.jpg".to_string());
+        expected
+            .insert("homepage/drankdozijn/drankdozijn-enjoyislay-de-dd-lagavulin.jpg".to_string());
+
+        assert_eq!(
+            candidates("homepage/drankdozijn/drankdozijn-enjoyislay-de-lagavulin8.jpg"),
+            expected,
+        );
+    }
+
+    #[test]
+    fn tests_candidates_with_multi_digit_number() {
+        let mut expected = HashSet::new();
+
+        expected.insert(
+            "homepage/drankdozijn/drankdozijn-enjoyislay-de-dd-lagavulin12.jpg".to_string(),
+        );
+        expected
+            .insert("homepage/drankdozijn/drankdozijn-enjoyislay-de-dd-lagavulin.jpg".to_string());
+
+        assert_eq!(
+            candidates("homepage/drankdozijn/drankdozijn-enjoyislay-de-lagavulin12.jpg"),
+            expected
+        );
+    }
+
+    #[test]
+    fn tests_candidates_with_dash_and_number() {
+        let mut expected = HashSet::new();
+
+        expected.insert(
+            "homepage/drankdozijn/drankdozijn-enjoyislay-de-dd-lagavulin-8.jpg".to_string(),
+        );
+        expected
+            .insert("homepage/drankdozijn/drankdozijn-enjoyislay-de-dd-lagavulin.jpg".to_string());
+
+        assert_eq!(
+            candidates("homepage/drankdozijn/drankdozijn-enjoyislay-de-lagavulin-8.jpg"),
+            expected,
+        );
+    }
+
+    #[test]
+    fn tests_candidates_with_dash_after_number() {
+        let mut expected = HashSet::new();
+
+        expected.insert(
+            "homepage/drankdozijn/drankdozijn-enjoyislay-de-dd-lagavulin-8-.jpg".to_string(),
+        );
+        expected
+            .insert("homepage/drankdozijn/drankdozijn-enjoyislay-de-dd-lagavulin.jpg".to_string());
+
+        assert_eq!(
+            candidates("homepage/drankdozijn/drankdozijn-enjoyislay-de-lagavulin-8-.jpg"),
+            expected,
+        );
     }
 }
